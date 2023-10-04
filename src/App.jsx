9ddf2e987f8c2aaf85e4f37d885f91cf38840a94
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-04 18:07:04 +0300

    teoria osasta 2

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Note from "./components/Notes/Note";
+
+
+const App = ({ notes }) => {
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+      {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
