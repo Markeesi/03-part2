a9e694d46679dfbc2a54bb82dd18715495c00c95
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-05 22:24:59 +0300

    päivitys

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import Note from "./components/Notes/Note";
+import { useState } from "react";
+
+const App = (props) => {
+
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
 
 
-const App = ({ notes }) => {
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length +1,
+    }
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  }
 
   return (
     <div>
@@ -11,6 +33,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 };
