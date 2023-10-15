34fcac2a597cce249f0cd9629689d7519120699b
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-15 23:36:34 +0300

    teoria osuus tehty

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import Note from "./components/Notes/Note";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = (props) => {
+const App = () => {
 
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true)
+
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
 
   const addNote = (event) => {
